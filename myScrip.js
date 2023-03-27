1bc3c9d99cdb692e4b983948d80a2454c2228cac
Zaire <110312083+zhar1@users.noreply.github.com>
2023-03-27 15:24:30 -0500

    create basic operator functions

---
+++ myScrip.js
@@ -0,0 +1,20 @@
+const add = function(a, b){
+    return a + b;
+}
+const subtract = function(a, b){
+    return a - b;
+}
+const multiply = function(a, b){
+    return a * b;
+}
+const divide = function(a, b){
+    if(b === 0){
+        return 'sike';
+    }
+    else 
+        return a / b;
+}
+
+function operate (a, operator, b){
+    
+}
