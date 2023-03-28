a215c9ee9b28df588510040052a3acf18bd7138d
Zaire <110312083+zhar1@users.noreply.github.com>
2023-03-27 21:49:47 -0500

    creat allClear function, clears variables/display

--- myScrip.js
+++ myScrip.js
@@ -1,3 +1,16 @@
+let firstNum = 0;
+let lastNum = 0;
+let operator = '';
+// function temp(e){
+//     console.log(e);
+// }
+const buttons = document.querySelectorAll('.key');
+const display = document.querySelector('.calcScreen');
+const operatorArray = ['=', '+', '/', '*', '-'];
+//creat nodelist for operators
+const opList = document.querySelectorAll('.operator');
+
+
 const add = function(a, b){
     return a + b;
 }
@@ -27,21 +40,25 @@ const operate  = function(a, operator, b){
             return divide(a, b);
     }
 }
+const allClear = function(){
+    firstNum = null;
+    lastNum = null;
+    operator = null;
+    display.textContent = '';
+}
 //variables
-let firstNum = 0;
-let lastNum = 0;
-let operator = '';
-// function temp(e){
-//     console.log(e);
-// }
-const buttons = document.querySelectorAll('.key');
-const display = document.querySelector('.calcScreen');
-const operatorArray = ['=', '+', '/', '*', '-'];
-//creat nodelist for operators
-const opList = document.querySelectorAll('.operator')
+
+
+//event listeners
+opList.forEach(operator => operator.addEventListener('click', () => {
+    display.innerText = '';
+}));
+
 buttons.forEach(button => button.addEventListener('click', () => {
-    console.log(button.innerText);
+    // console.log(button.innerText);
     display.textContent += `${button.innerText}`;
 
 }));
+
+//next step 
 
