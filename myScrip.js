2fca76e6a75a7fc44f5802a123f0b9e714df6481
Zaire <110312083+zhar1@users.noreply.github.com>
2023-04-04 12:20:43 -0500

    fix bug where last number did not update

--- myScrip.js
+++ myScrip.js
@@ -3,13 +3,15 @@ let displayValue = '0';
 let firstNum = null;
 let lastNum = null;
 let symbol = null;
+const operatorString = '+-/*';
 
 const numbers = document.querySelectorAll('.number');
 const display = document.querySelector('.calcScreen');
 const operators = document.querySelectorAll('.operator');
 const allClearButton = document.getElementById('clear');
 const equalsButton = document.querySelector('.equal');
-
+const positiveNegative = document.querySelector('.positiveNegative');
+const percentButton = document.querySelector('.percent');
 
 
 //FUNCTIONS
@@ -57,32 +59,77 @@ function equal(){
         symbol = null;
     }
 }
-
+//function will update the display screen on the calculator
 function updateDisplay(someValue){
-
+    displayValue = someValue;
+    display.innerText = displayValue;
 }
-
+//function will make the current number positive or negative
+function changeSign(){
+    if(operatorString.includes(displayValue)){
+        return;
+    } else if(displayValue.includes('-')) {
+        let temp = displayValue.slice(1);
+        updateDisplay(temp);
+        updateNumber(temp);
+    } else if(displayValue) {
+        let temp = '-' + displayValue;
+        updateDisplay(temp);
+        updateNumber(temp);
+    }
+}
+//function will update thhe global operand values i.e firstNum and lastNum
+function updateNumber(someValue){
+   if(firstNum && !symbol && !lastNum){
+        firstNum = someValue;  
+    } else if(firstNum && symbol && lastNum){
+        lastNum = someValue;
+    }
+}
+//function will turn current number into a percentage
+function percentage(){
+    if(operatorString.includes(displayValue)){
+        return;
+    } else if(firstNum && !symbol && !lastNum){
+        let tempValue = Number(firstNum);
+        tempValue *= 0.1;
+        updateDisplay(tempValue.toString());
+        updateNumber(tempValue.toString());
+    } else if(firstNum && lastNum && symbol){
+        let tempValue = Number(lastNum);
+        tempValue *= 0.1;
+        updateDisplay(tempValue.toString());
+        updateNumber(tempValue.toString());
+    }
+}
 
 //event listeners 
 allClearButton.addEventListener('click', allClear);
 
 equalsButton.addEventListener('click', equal);
 
+positiveNegative.addEventListener('click', changeSign);
+
+percentButton.addEventListener('click', percentage);
+
+//event listeners with forEach loop
 operators.forEach(operator => operator.addEventListener('click', () => {
     const currentOperator = operator.innerText;
     //if only first number update the symbol(operator) to current operator and display it
     if (firstNum && !symbol && !lastNum){
         symbol = currentOperator;
-        display.innerText = currentOperator;
+        updateDisplay(symbol);
     } else if (firstNum && symbol && !lastNum){
-        display.innerText = operate(firstNum, symbol, firstNum);
+        const result = operate(firstNum, symbol, firstNum);
+        updateDisplay(result);
         lastNum = null;
-        firstNum = display.innerText;
+        firstNum = displayValue;
         symbol = currentOperator;
     } else if (firstNum && lastNum && symbol){
-        display.innerText = operate(firstNum, symbol, lastNum);
+        const result = operate(firstNum, symbol, lastNum);
+        updateDisplay(result);
         lastNum = null;
-        firstNum = display.innerText;
+        firstNum = displayValue;
         symbol = currentOperator;
     }
 }));
@@ -91,22 +138,15 @@ numbers.forEach(number => number.addEventListener('click', () => {
     const currentNumber = number.innerText;
     if(!firstNum && !symbol && !lastNum){
         firstNum = currentNumber;
-        display.textContent = currentNumber;
+        updateDisplay(firstNum);
     } else if(firstNum && !symbol && !lastNum){
         firstNum += currentNumber;
-        display.textContent += currentNumber;
+        updateDisplay(firstNum);
     } else if(firstNum && symbol && !lastNum){
         lastNum = currentNumber;
-        display.textContent = currentNumber;
+        updateDisplay(lastNum);
     } else {
         lastNum += currentNumber;
-        display.textContent += currentNumber;
+        updateDisplay(lastNum);
     }
-    
-
 }));
-
-//next step 
-
-// make a function to update display and variables
-
